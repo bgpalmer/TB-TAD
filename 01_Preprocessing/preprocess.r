ec7eb950e9ea8e3d3726b2a34ecf829f343709f6
Brian Palmer <bgpalmer14@gmail.com>
2022-02-25 17:49:44 -0800

    Forgot changes to preprocess r file.

--- 01_Preprocessing/preprocess.r
+++ 01_Preprocessing/preprocess.r
@@ -7,32 +7,32 @@ library(data.table)
 library(stringr)
 library(plyr)
 
-# #Get TAD boundaries from TAD file
+#Get TAD boundaries from TAD file
 # TAD<- read_table(paste0(Sys.getenv('DATADIR'), "/Hi-C/GSM455133_30E0LAAXX.1.maq.hic.summary.binned.txt"),col_names = F)
 TAD<- read_table(paste0(Sys.getenv('DATADIR'), "/TADs/GM12878_Lieberman-raw_TADs.txt"), col_names = F)
 # head(TAD)
-# #colnames(TAD)[1:3]<- c("X1","X2","X3") #chr, start, stop
-# bdsdf <-matrix("0",nrow=nrow(TAD),ncol=3)
-# colnames(bdsdf)<-c("seqnames","start","end")
-# head(bdsdf)
-# for(i in 1:(nrow(TAD)-1)){
-#   if(TAD[i,"X1"] == TAD[i+1,"X1"])
-#     {
-#     bdsdf[i,"seqnames"]<-as.character(TAD[i,"X1"])
-#     bdsdf[i,"start"]<-(as.numeric(TAD[i,"X3"])+as.numeric(TAD[i+1,"X2"])+1)/2 - 100000 #boundary width is 200k
-#     bdsdf[i,"end"]<-(as.numeric(TAD[i,"X3"])+as.numeric(TAD[i+1,"X2"])+1)/2 + 100000
-#     }
-# }
+# nrow(TAD)
+#colnames(TAD)[1:3]<- c("X1","X2","X3") #chr, start, stop
+bdsdf <-matrix("0",nrow=nrow(TAD),ncol=3)
+colnames(bdsdf)<-c("seqnames","start","end")
+for(i in 1:(nrow(TAD)-1)){
+  if(TAD[i,"X1"] == TAD[i+1,"X1"])
+    {
+    bdsdf[i,"seqnames"]<-as.character(TAD[i,"X1"])
+    bdsdf[i,"start"]<-(as.numeric(TAD[i,"X3"])+as.numeric(TAD[i+1,"X2"])+1)/2 - 100000 #boundary width is 200k
+    bdsdf[i,"end"]<-(as.numeric(TAD[i,"X3"])+as.numeric(TAD[i+1,"X2"])+1)/2 + 100000
+    }
+}
 
-# bdsdf<-bdsdf[-(which(bdsdf[,"seqnames"]=="0")),] #remove the rows inbetween chromosomes
+bdsdf<-bdsdf[-(which(bdsdf[,"seqnames"]=="0")),] #remove the rows inbetween chromosomes
+bdsGR <- GRanges(seqnames = bdsdf[,"seqnames"],
+                      ranges = IRanges(start=as.numeric(bdsdf[,"start"]),end=as.numeric(bdsdf[,"end"])),
+                      strand='*')
+                      
+bdsGR_GM12878 <- bdsGR
 
 # save(list=ls(),file='bdsdf.Rdata')
 
-# bdsGR <- GRanges(seqnames = bdsdf[,"seqnames"],
-#                       ranges = IRanges(start=as.numeric(bdsdf[,"start"]),end=as.numeric(bdsdf[,"end"])),
-#                       strand='*')
-                      
-# bdsGR_GM12878 <- bdsGR
 
 
 #######################################
@@ -67,6 +67,12 @@ probGR<-GRanges(seqnames=(prob_data_all$seqnames)
 me <- probGR
 
 chrnames<-c(paste('chr',1:22,sep=''),'chrX')
+
+
+
+##get center and chr_size information for each chrs
+chr_info_1<-read_tsv(paste0(Sys.getenv('DATADIR'),'/Sizes/hg19_chrom_centros.txt'), col_names = F)
+chr_info_2<-read_tsv(paste0(Sys.getenv('DATADIR'),'/Sizes/hg19_chrom_sizes.txt'),col_names = F)
 
 
 # #######################################
